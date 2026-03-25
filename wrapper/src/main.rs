1c7ba4b42ac3eee726051dfb0460abaad0240a96
Kaspar Lyngsie <github@pacroon.dk>
2026-03-25 21:52:24 +0100

    fix: added a basic main

--- wrapper/src/main.rs
+++ wrapper/src/main.rs
@@ -1,3 +1,22 @@
+mod config;
+mod http_server;
+
+use tracing::{debug, error, info};
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+        // Get log level from RUST_LOG or default to "INFO"
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("info".parse().unwrap()),
+        )
+        .json()
+        // Promote fields to top level, instead of nesting inside 'fields'
+        .flatten_event(true)
+        .init();
+
+    info!(
+        port = config::PROXY_LISTEN_PORT,
+        "starting MeiliSearch wrapper proxy"
+    );
 }
